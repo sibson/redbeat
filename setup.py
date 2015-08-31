fa007302dd76d9adc9a8f330e40995a9a21499a3
Marc Sibson <sibson@gmail.com>
2015-08-30 20:36:57 -0700

    cleanup packaging

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name="celery-redbeat",
     description="A Celery Beat Scheduler using Redis for persistent storage",
     version="0.8.0",
+    url="https://github.com/sibson/redbeat",
     license="Apache License, Version 2.0",
     author="Marc Sibson",
-    author_email="sibson@gmail.com",
-    keywords="python celery beat redis",
+    author_email="sibson+redbeat@gmail.com",
+    keywords="python celery beat redis".split(),
     packages=[
         "redbeat"
     ],
