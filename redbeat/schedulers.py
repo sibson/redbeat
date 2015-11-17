fc4a5f276d0e3a3b8c7bba0bbe2ce473444fb3dc
Marc Sibson <sibson@gmail.com>
2015-11-16 20:38:30 -0800

    remove deleted entries on restart
        - fixes #11

--- redbeat/schedulers.py
+++ redbeat/schedulers.py
@@ -24,6 +24,8 @@ from decoder import RedBeatJSONEncoder, RedBeatJSONDecoder
 rdb = StrictRedis.from_url(current_app.conf.REDBEAT_REDIS_URL)
 
 REDBEAT_SCHEDULE_KEY = current_app.conf.REDBEAT_KEY_PREFIX + ':schedule'
+REDBEAT_STATICS_KEY = current_app.conf.REDBEAT_KEY_PREFIX + ':statics'
+
 
 ADD_ENTRY_ERROR = """\
 
@@ -134,8 +136,19 @@ class RedBeatScheduler(Scheduler):
     Entry = RedBeatSchedulerEntry
 
     def setup_schedule(self):
+        # cleanup old static entries
+        previous = rdb.smembers(REDBEAT_STATICS_KEY)
+        current = set(self.app.conf.CELERYBEAT_SCHEDULE.keys())
+        removed = previous - current
+        for name in removed:
+            RedBeatSchedulerEntry(name).delete()
+
+        # setup statics
         self.install_default_entries(self.app.conf.CELERYBEAT_SCHEDULE)
         self.update_from_dict(self.app.conf.CELERYBEAT_SCHEDULE)
+
+        # track static entries
+        rdb.sadd(REDBEAT_STATICS_KEY, *self.app.conf.CELERYBEAT_SCHEDULE.keys())
 
     def update_from_dict(self, dict_):
         for name, entry in dict_.items():
