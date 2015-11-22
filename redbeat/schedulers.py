98c089e5b206306b4bf17a42cd48e8e0d6cb7175
Marc Sibson <sibson@gmail.com>
2015-11-21 20:44:24 -0800

    add conftest for py.test magic PYTHONPATH

--- redbeat/schedulers.py
+++ redbeat/schedulers.py
@@ -1,7 +1,7 @@
 # Licensed under the Apache License, Version 2.0 (the 'License'); you may not
 # use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
-# Copyright 2014 Kong Luoxing, Copyright 2015 Marc Sibson
+# Copyright 2015 Marc Sibson
 
 
 import datetime
