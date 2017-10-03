2c4005d342d4793befcdcabf01d0f7bb32a6965d
Marc Sibson <sibson@gmail.com>
2017-10-03 10:51:38 -0400

    reset default exampleconf

--- exampleconf.py
+++ exampleconf.py
@@ -1,4 +1,4 @@
-BROKER_URL = 'redis://foo:base@localhost:2323'
+BROKER_URL = 'redis://'
 
 CELERYBEAT_SCHEDULER = 'redbeat.RedBeatScheduler'
 CELERYBEAT_MAX_LOOP_INTERVAL = 5  # redbeat likes fast loops
