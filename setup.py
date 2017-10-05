8dfcf0f388256eaf9a3847010dbff213e09c0c2b
Marc Sibson <sibson@gmail.com>
2017-10-05 11:10:05 -0400

    bump version to 0.10.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="celery-redbeat",
     description="A Celery Beat Scheduler using Redis for persistent storage",
     long_description=long_description,
-    version="0.10.2",
+    version="0.10.3",
     url="https://github.com/sibson/redbeat",
     license="Apache License, Version 2.0",
     author="Marc Sibson",
