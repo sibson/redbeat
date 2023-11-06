28efe303c686597347f559092235303a2fa85f15
David Francos <opensource@davidfrancos.net>
2023-11-06 12:51:16 +0100

    fix: #260 Use from_url instead of specifying manually the config

--- redbeat/schedulers.py
+++ redbeat/schedulers.py
@@ -137,10 +137,11 @@ def get_redis(app=None):
             connection = StrictRedis.from_url(conf.redis_url, decode_responses=True, **ssl_options)
         elif conf.redis_url.startswith('redis-cluster'):
             from redis.cluster import RedisCluster
-
-            if not redis_options.get('startup_nodes'):
-                redis_options = {'startup_nodes': [{"host": "localhost", "port": "30001"}]}
-            connection = RedisCluster(decode_responses=True, **redis_options)
+            connection = RedisCluster.from_url(
+                conf.redis_url.replace("redis-cluster://", "redis://"),
+                decode_responses=True, 
+                **redis_options,
+            )
         else:
             connection = StrictRedis.from_url(conf.redis_url, decode_responses=True)
 
